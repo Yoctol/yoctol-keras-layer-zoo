2e3a282b7f4783e5655c079ca4fdb776aeea7820
plliao <r02921027@ntu.edu.tw>
2017-06-12 18:54:49 +0800

    fix testcases with MaskConvNet

--- test/util/test_flatten.py
+++ test/util/test_flatten.py
@@ -6,8 +6,9 @@ import keras.backend as K
 from keras.models import Input, Model
 from keras.utils.conv_utils import conv_output_length
 from keras.layers.pooling import MaxPool2D
+from keras.layers import Conv2D
 
-from yklz import Convolution2D, MaskPooling
+from yklz import MaskConvNet, MaskPooling
 from yklz import MaskConv, MaskFlatten
 from test import TestBase2DClass
 
@@ -43,10 +44,12 @@ class TestMaskFlattenClass(TestBase2DClass, TestCase):
     def create_model(self):
         inputs = Input(shape=(self.x, self.y, self.channel_size))
         masked_inputs = MaskConv(self.mask_value)(inputs)
-        conv_outputs = Convolution2D(
-            self.filters,
-            self.kernel,
-            self.strides
+        conv_outputs = MaskConvNet(
+            Conv2D(
+                self.filters,
+                self.kernel,
+                strides=self.strides
+            )
         )(masked_inputs)
         pooling_outputs = MaskPooling(
             MaxPool2D(
