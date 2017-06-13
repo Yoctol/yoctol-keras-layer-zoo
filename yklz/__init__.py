b2c13b2d6f775df2cb43ca4ce68b5df074d2af0f
plliao <r02921027@ntu.edu.tw>
2017-06-13 10:45:50 +0800

    change LSTM folder to recurrent

--- yklz/__init__.py
+++ yklz/__init__.py
@@ -1,13 +1,11 @@
-'''Customed LSTM Layers'''
-from .LSTM.lstm_peephole import LSTMPeephole
-from .LSTM.lstm_decoder import LSTMDecoder
-from .LSTM.lstm_encoder import LSTMEncoder
-from .LSTM.lstm_cell import LSTMCell
-
 '''Customed recurrent wrappers'''
 from .recurrent.bidirectional_rnn_encoder import BidirectionalRNNEncoder
 from .recurrent.rnn_encoder import RNNEncoder
 from .recurrent.rnn_decoder import RNNDecoder
+from .recurrent.lstm_peephole import LSTMPeephole
+from .recurrent.lstm_decoder import LSTMDecoder
+from .recurrent.lstm_encoder import LSTMEncoder
+from .recurrent.lstm_cell import LSTMCell
 
 '''Customed convolution layers'''
 from .convolution.mask_ConvNet import MaskConvNet
