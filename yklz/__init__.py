1afaf859b7bb8e45339ef54fe30ea504c11b6680
plliao <r02921027@ntu.edu.tw>
2017-06-13 10:38:17 +0800

    move encoder and decoder to recurrent folder

--- yklz/__init__.py
+++ yklz/__init__.py
@@ -4,6 +4,11 @@ from .LSTM.lstm_decoder import LSTMDecoder
 from .LSTM.lstm_encoder import LSTMEncoder
 from .LSTM.lstm_cell import LSTMCell
 
+'''Customed recurrent wrappers'''
+from .recurrent.bidirectional_rnn_encoder import BidirectionalRNNEncoder
+from .recurrent.rnn_encoder import RNNEncoder
+from .recurrent.rnn_decoder import RNNDecoder
+
 '''Customed convolution layers'''
 from .convolution.mask_ConvNet import MaskConvNet
 from .convolution.convEncoder import ConvEncoder
@@ -19,7 +24,4 @@ from .util.padding_layer import PaddingZero
 from .util.flatten import MaskFlatten
 
 '''Wrapper Layers'''
-from .wrapper.bidirectional_rnn_encoder import BidirectionalRNNEncoder
 from .wrapper.mask_to_seq import MaskToSeq
-from .wrapper.rnn_encoder import RNNEncoder
-from .wrapper.rnn_decoder import RNNDecoder
